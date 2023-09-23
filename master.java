64760917bf7fda4220fe1099accee7c2fa3a916e
sithick772 <144975571+sithick772@users.noreply.github.com>
2023-09-23 14:01:22 +0530

    Create master.java

---
+++ master.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class master
+  {
+    public static void main(String[] args)
+    {
+      Double num;
+      Scanner sc = new Scanner (System.in);
+      System.out.println("Enter a Number");
+      num = sc.nextDouble();
+      Double square = num*num;
+      System.out.println("The value od Square is :" + square);
+    }
+    
+  }
