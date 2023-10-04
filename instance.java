d7e1769ff9dee44ef514df7de40e4775fa505ec8
sithick772 <glitstones772@gmail.com>
2023-10-04 13:08:12 +0530

    new file added

---
+++ instance.java
@@ -0,0 +1,10 @@
+public class instance 
+{
+    int b= 3000;
+    public static void main(String args[])
+    {
+        instance obj=new instance();
+        System.out.println(obj.b);
+    }
+    
+}
